c07ed2c9603143cccda39b9a67cf761bd022c438
Jens Christian Steenvinkel Pedersen <steenvinkel@users.noreply.github.com>
2019-07-03 17:37:37 +0200

    Added Legacy SubCategory controller

---
+++ Purchases/Controllers/LegacySubCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Purchases.Helpers;
+
+namespace Purchases.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LegacySubCategoryController : ControllerBase
+    {
+        private readonly PurchasesContext _context;
+
+        public LegacySubCategoryController(PurchasesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            var userId = HttpContext.GetUserId();
+            var subcategoryNames = from subcategory in _context.Subcategory
+                                   join category in _context.Category on subcategory.CategoryId equals category.CategoryId
+                                   where category.UserId == userId
+                                   select subcategory.Name;
+
+            return Ok(subcategoryNames);
+        }
+    }
+}
