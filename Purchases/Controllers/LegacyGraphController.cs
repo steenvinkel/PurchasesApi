74e695f726dc7032f4f8a636f8f2155bedc80fbb
Jens Christian Steenvinkel Pedersen <steenvinkel@users.noreply.github.com>
2019-07-04 13:29:15 +0200

    Added endpoint for Summary

--- Purchases/Controllers/LegacyGraphController.cs
+++ Purchases/Controllers/LegacyGraphController.cs
@@ -19,6 +19,54 @@ namespace Purchases.Controllers
             _context = context;
         }
 
+        [HttpGet("Summary")]
+        public ActionResult Summary()
+        {
+            var userId = HttpContext.GetUserId();
+
+            var categories = _context.Category
+                .Where(c => c.UserId == userId)
+                .Select(c => new { c.Name, CategoryId = c.CategoryId.ToString() })
+                .ToList();
+
+            var subCategories = _context.Subcategory
+                .Join(_context.Category, s => s.CategoryId, c => c.CategoryId, (s, c) => new { s, c.UserId })
+                .Where(r => r.UserId == userId)
+                .Select(pair => new
+                {
+                    pair.s.Name,
+                    SubcategoryId = pair.s.SubcategoryId.ToString(),
+                    CategoryId = pair.s.CategoryId.ToString(),
+                })
+                .ToList();
+
+            var summary =
+                (from posting in _context.Posting
+                 join subcategory in _context.Subcategory on posting.SubcategoryId equals subcategory.SubcategoryId
+                 join category in _context.Category on subcategory.CategoryId equals category.CategoryId
+                 where posting.UserId == userId
+                 group posting.Amount by new { posting.Date.Year, posting.Date.Month, subcategory.SubcategoryId, category.CategoryId } into g
+                 select new {
+                     g.Key.Year,
+                     g.Key.Month,
+                     g.Key.SubcategoryId,
+                     g.Key.CategoryId,
+                     Sum = g.Sum()
+                 } ).ToList();
+
+            var summaryMap = summary
+                .GroupBy(s => s.CategoryId).ToDictionary(gc => gc.Key,
+                    gc => gc.GroupBy(s => s.SubcategoryId).ToDictionary(gs => gs.Key,
+                        gs => gs.GroupBy(s => s.Year).ToDictionary(gy => gy.Key,
+                            gy => gy.GroupBy(s => s.Month).ToDictionary(gm => gm.Key, gm => gm.Sum(x => x.Sum)
+                            )
+                        )
+                    )
+                );
+
+            return Ok(summaryMap);
+        }
+
         [HttpGet("NumDailyPurchases")]
         public ActionResult NumDailyPurchases()
         {
