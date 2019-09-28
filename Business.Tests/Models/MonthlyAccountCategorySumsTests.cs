5aeb9e369d587bf616acb6fbd4cc1a3375226e21
Jens Christian Steenvinkel Pedersen <steenvinkel@users.noreply.github.com>
2019-09-28 14:43:30 +0200

    Added more unit tests

--- Business.Tests/Models/MonthlyAccountCategorySumsTests.cs
+++ Business.Tests/Models/MonthlyAccountCategorySumsTests.cs
@@ -1,11 +1,24 @@
 ﻿using Business.Models;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace Business.Tests.Dashboard
 {
     public class MonthlyAccountCategorySumsTests
     {
+
+        [TestCase]
+        public void Constructor_NoParameters_ShouldGiveEmptyDictionary()
+        {
+            var sut = new MonthlyAccountCategorySums();
+
+            var count = sut.Count;
+
+            Assert.AreEqual(0, count);
+
+        }
+
         private static object[] FortuneAndInvestmentScenarios = new object[] 
         {
             new object[]{ new Dictionary<MonthAndYear, Dictionary<string, double>>(), new MonthAndYear(2019, 9), 0 },
@@ -35,5 +48,55 @@ namespace Business.Tests.Dashboard
 
             Assert.AreEqual(expectedSum, fortune);
         }
+
+
+        private static object[] OwnPensionSavingsScenarios = new object[] 
+        {
+            new object[]{ new Dictionary<MonthAndYear, Dictionary<string, double>>
+                { {new MonthAndYear(2019,9), new Dictionary<string, double> { {"Pension", 2000 } } } },
+                new MonthAndYear(2019, 9), 2000
+            },
+            new object[]{ new Dictionary<MonthAndYear, Dictionary<string, double>>
+                {
+                    { new MonthAndYear(2019,8), new Dictionary<string, double> { {"Pension", 1000 } } },
+                    { new MonthAndYear(2019,9), new Dictionary<string, double> { {"Pension", 2000 } } }
+                },
+                new MonthAndYear(2019, 9), 1000
+            },
+            new object[]{ new Dictionary<MonthAndYear, Dictionary<string, double>>
+                {
+                    { new MonthAndYear(2019,8), new Dictionary<string, double> { {"Pension", 4000 } } },
+                    { new MonthAndYear(2019,9), new Dictionary<string, double> { {"Pension", 2000 } } }
+                },
+                new MonthAndYear(2019, 9), -2000
+            },
+            new object[]{ new Dictionary<MonthAndYear, Dictionary<string, double>>
+                {
+                    { new MonthAndYear(2019,8), new Dictionary<string, double> { { "Fortune", 2000 }, { "Investment", 2000 }, {"Pension", 5000 } } },
+                    { new MonthAndYear(2019,9), new Dictionary<string, double> { { "Fortune", 2000 }, { "Investment", 2000 }, {"Pension", 9000 } } }
+                },
+                new MonthAndYear(2019, 9), 4000
+            },
+        };
+        [TestCaseSource(nameof(OwnPensionSavingsScenarios))]
+        public void GetOwnPensionSavings_DifferentScenarios_ShouldReturnCorrectValue(Dictionary<MonthAndYear, Dictionary<string, double>> dictionary, MonthAndYear monthAndYear, double expectedPension)
+        {
+            var sut = new MonthlyAccountCategorySums(dictionary);
+
+            var ownPension = sut.GetOwnPensionSavings(monthAndYear);
+
+            Assert.AreEqual(expectedPension, ownPension);
+        }
+
+
+        [TestCase]
+        public void GetOwnPensionSavings_IfMonthCannotBeFound_ThrowException()
+        {
+            var sut = new MonthlyAccountCategorySums(new Dictionary<MonthAndYear, Dictionary<string, double>>());
+
+            var monthAndYear = new MonthAndYear(2019, 9);
+            Assert.Catch<Exception>(() => sut.GetOwnPensionSavings(monthAndYear));
+        }
+
     }
 }
