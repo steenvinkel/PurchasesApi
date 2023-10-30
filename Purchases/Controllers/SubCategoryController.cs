641221bb6d080203f88c1f69dbf02e41f939509e
Jens Christian Steenvinkel Pedersen <steenvinkel@users.noreply.github.com>
2023-10-30 15:03:52 +0100

    Added SubCategories controller

---
+++ Purchases/Controllers/SubCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Business.Models;
+using Business.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Purchases.Helpers;
+
+namespace Purchases.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoryController : ControllerBase
+    {
+        private readonly ISubCategoryRepository _subCategoryRepository;
+
+        public SubCategoryController(ISubCategoryRepository subCategoryRepository)
+        {
+            _subCategoryRepository = subCategoryRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<List<SubCategory>> Get()
+        {
+            var userId = HttpContext.GetUserId();
+
+            var subcategories = _subCategoryRepository.GetList(userId);
+
+            return Ok(subcategories);
+        }
+    }
+}
