7c8a18390460cbf3e84bd2f660abf066a5d9f326
Jens Christian Steenvinkel Pedersen <steenvinkel@users.noreply.github.com>
2024-10-27 17:14:47 +0100

    Upgraded from .Net 7 to 8

--- Purchases/Startup.cs
+++ Purchases/Startup.cs
@@ -17,14 +17,9 @@ using Purchases.Middleware;
 
 namespace Purchases
 {
-    public class Startup
+    public class Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; } = configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
